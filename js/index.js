f71b26d8b0f00d93d41fa6277cf688c8c2c94295
Brian Arango <brianvictus@gmail.com>
2019-07-30 16:08:39 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-let element = document.createElement('div');
-
-element.innerHTML = 'Hey, how are ya?';
-element.style.background = '#f9f9f9';
+let element = document.createElement('div')
+element.innerHTML = 'Hello, DOM!'
+element.style.background = '#f9f9f9'
 
 document.body.appendChild(element);
+element.style.textAlign = 'center';
 
-ul.removeChuld(ul.querySelector('li:nth-child(2)'));
+let ul = document.createElement('ul');
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+element.appendChild(ul);
+ul.style.textAlign = 'left'
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
